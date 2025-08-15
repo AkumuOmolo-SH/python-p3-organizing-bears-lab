bc39b2df6990739ac304f26f5a94ea24240bb8c1
AkumuOmolo <akumu.omoloh@gmail.com>
2025-08-15 17:42:10 +0300

    Adds sql codegrade

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr Chocolate", 4, "M", "Brown", "Aggressive", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 6, "M", "White", "Calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 3, "F", "Brown", "Aggressive", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 4, "M", "Brown", "Aggressive", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 6, "M", "White", "Calm", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 3, "F", "Brown", "Aggressive", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 5, "M", "White", "Calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 3, "F", "Black", "Aggressive", 0);
